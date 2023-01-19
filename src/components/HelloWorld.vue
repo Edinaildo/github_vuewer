3043ec04797c96870efeb01c87d16e61b998c9f7
Edinaildo <edinaildopereira2013@gmail.com>
2023-01-19 14:04:52 -0300

    teste deploy automatico

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -112,7 +112,7 @@
       ],
       importantLinks: [
         {
-          text: 'Documentation',
+          text: 'Documentationnnnnnnn',
           href: 'https://vuetifyjs.com',
         },
         {
